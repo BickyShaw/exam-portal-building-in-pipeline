bbc445106c0c46075bd614ee51859ba57d7247fe
Mithungith <88321089+Mithungith@users.noreply.github.com>
2022-02-28 20:18:44 +0530

    Just a Begin
    Just push the code, so team can see this

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function App(){
+    return <div className="main-section">
+        <div className="question-section">Question</div>
+        <div className="qNumber-section">Question-numbers</div>
+    </div>
+}
+
+export default App;
