5c7f3c57ff1603ac45b99f6948075616c8188751
Mithungith <88321089+Mithungith@users.noreply.github.com>
2022-04-29 20:49:51 +0530

    Added updated files

---
+++ src/ImpBtn.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import Qsec from "./Qsec";
+// import updateQues from "./Qsec"
+
+function Impbtn(){
+    return <div className="important-btn">
+    <button className="mark-question-btn">Mark</button>
+    <button className="back-btn">Back</button>
+    <button className="submit-btn" 
+    // name={changeq}
+    >Next</button>
+    </div>
+}
+
+export default Impbtn;
+// export {changeq};
